cea2e6dbd8fec2c6d387cbe65459adf7d6e324cc
ticasali <ticasali83@gmail.com>
2025-08-03 16:17:17 +0200

    Rebuild Parsing Struct

--- src/parsing/load_data.c
+++ src/parsing/load_data.c
@@ -1,34 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   load_data.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ticasali <ticasali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/03 10:02:30 by ticasali          #+#    #+#             */
+/*   Created: 2025/08/03 16:11:30 by ticasali          #+#    #+#             */
 /*   Updated: 2025/08/03 16:16:02 by ticasali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-#define PARSING_H
-#include <stdbool.h>
+#include "../include/modern_warfare_7.h"
 
-typedef	struct	s_data
-{
-	void			*north;
-	void 			*south;
-	void			*west;
-	void			*east;
-	unsigned char	*floor;
-	unsigned char	*sky;
-	char			**map;
-}	t_data;
-
-char	**check_file(char *pathname);
-
-bool	check_parsing(char *pathname, t_data *data);
-bool	check_arg(int ac, char **av);
-bool	check_cub_extension(char *str);
-
-#endif
