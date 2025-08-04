6febd8c32770a6d4d83b71cff2e0f9c239d05b43
ticasali <ticasali@gmail.com>
2025-08-04 18:35:52 +0200

    Add_color_parsing

--- src/parsing/pars_line_for_data.c
+++ src/parsing/pars_line_for_data.c
@@ -6,7 +6,7 @@
 /*   By: ticasali <ticasali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:05:15 by ticasali          #+#    #+#             */
-/*   Updated: 2025/08/04 17:21:50 by ticasali         ###   ########.fr       */
+/*   Updated: 2025/08/04 18:26:32 by ticasali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,32 @@ void	*pars_line_for_image(char *line_to_pars, t_window *win)
 	while (line_to_pars[x] == ' ')
 		++x;
 	return (load_image(win, &line_to_pars[x], 64, 64));
+}
+
+unsigned char	*pars_line_for_color(char *line_to_pars)
+{
+	unsigned char	*ret;
+
+	ret = malloc(sizeof(unsigned char) * 3);
+	if (ret == NULL)
+		return (NULL);
+	size_t	x = 1;
+	while (line_to_pars[x] == ' ')
+		++x;
+	ret[0] = atorgb(&line_to_pars[x]);
+	while (line_to_pars[x] != '\0' && line_to_pars[x] != ' ')
+		++x;
+	if (line_to_pars[x] != '\0')
+		return (NULL);
+	while (line_to_pars[x] == ' ')
+		++x;
+	ret[1] = atorgb(&line_to_pars[x]);
+	while (line_to_pars[x] != '\0' || line_to_pars[x] != ' ')
+		++x;
+	if (line_to_pars[x] != '\0')
+		return (NULL);
+	while (line_to_pars[x] == ' ')
+		++x;
+	ret[2] = atorgb(&line_to_pars[x]);
+	return (ret);
 }
--- src/utils/convert.c
+++ src/utils/convert.c
@@ -6,7 +6,7 @@
 /*   By: ticasali <ticasali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 10:32:43 by ticasali          #+#    #+#             */
-/*   Updated: 2025/08/04 10:32:56 by ticasali         ###   ########.fr       */
+/*   Updated: 2025/08/04 18:31:07 by ticasali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,23 @@ double	atod(char *str)
 	}
 	ret += (double)(str[++ct] - 48) / 10;
 	ret += (double)(str[++ct] - 48) / 100;
+	return (ret);
+}
+
+unsigned char	atorgb(char *str)
+{
+	unsigned char	ret;
+	size_t			check;
+	size_t			ct;
+
+	ret = 0;
+	ct = 0;
+	while ((str[ct] > 47 && str[ct] < 58) && ct < 3)
+	{
+		ret += str[ct] - 48;
+		if ((str[++ct] > 47 && str[ct] < 58))
+			ret *= 10;
+	//DONT FORGET TO SET ERNNO FOR ERROR CASE IN PARSING
+	}
 	return (ret);
 }
