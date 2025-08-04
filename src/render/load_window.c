688d1dd90388c517fc8431d22bef88b2ddee6c57
ticasali <ticasali@gmail.com>
2025-08-04 10:48:51 +0200

    Start Principal Structure and the Body of the Project

--- src/render/load_window.c
+++ src/render/load_window.c
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   game.c                                             :+:      :+:    :+:   */
+/*   load_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ticasali <ticasali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/03 17:59:00 by ticasali          #+#    #+#             */
-/*   Updated: 2025/08/04 10:48:16 by ticasali         ###   ########.fr       */
+/*   Created: 2025/08/04 10:42:01 by ticasali          #+#    #+#             */
+/*   Updated: 2025/08/04 10:42:42 by ticasali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/modern_warfare_7.h"
+
+bool	load_window(t_window	*wind)
+{
+	wind->ml = mlx_init();
+	if (wind->ml == NULL)
+		return (false);
+	wind->win = mlx_new_window(wind->ml, 3072, 1728, "Mordern Warfare 7");	//1728
+	if (wind->win == NULL)
+		return (false);
+	return (true);
+}
