328e97d72ff167cec620f40d03c8e5255c951d3c
ticasali <ticasali@gmail.com>
2025-09-09 20:00:12 +0200

    Start Opti Transparency Pixel

--- src/home/home_render.c
+++ src/home/home_render.c
@@ -6,7 +6,7 @@
 /*   By: ticasali <ticasali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 12:47:38 by ticasali          #+#    #+#             */
-/*   Updated: 2025/09/09 17:14:54 by ticasali         ###   ########.fr       */
+/*   Updated: 2025/09/09 19:59:20 by ticasali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,25 @@ int	home_render(t_control *ctrl)
 {
 	double	time;
 	double	test;
+	void	*back;
+	void	*img;
 
 	time = get_time();
-	test = 0.2;
+	test = 0.1;
 	if ((time - test) > ctrl->home->time)
 	{
+		//Try Buffer Image
+//		img = mlx_new_image(ctrl->win->ml, 1728, 960);
+//		back = load_image(ctrl->win, "./texture/back.xpm", 1728, 960);
+//		put_img_to_img(load_transparency(img, 1728, 960),
+//			load_transparency(back, 1728, 960), 0, 0);
+//		put_img_to_img(load_transparency(back, 1728, 960),
+//			load_transparency(ctrl->home->background->sprite[ctrl->home->background->frame_actual], 1728, 960), 0, 0);
+//	mlx_put_image_to_window(ctrl->win->ml, ctrl->win->win,
+//			back, 0, 0);
+//		mlx_destroy_image(ctrl->win->ml, img);
+//		mlx_destroy_image(ctrl->win->ml, back);
+		//
 		mlx_mouse_get_pos(ctrl->win->ml, ctrl->win->win,
 			&ctrl->home->x_mouse, &ctrl->home->y_mouse);
 //		home_animation_button(ctrl);
@@ -32,7 +46,6 @@ int	home_render(t_control *ctrl)
 			ctrl->home->background->sprite[ctrl->home->background->frame_actual], 0, 0);
 		increase_animation_frame(ctrl->home->background);
 	}
-	printf("--->%d\n", ctrl->home->statement);
 	if (ctrl->home->statement == 3 || ctrl->home->statement == 1)
 		mlx_loop_end(ctrl->win->ml);	
 	return (0);
